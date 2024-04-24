9eb7570a9d0e532c817ee67f812617cb7aa646d6
walidgeuttala <walidgeuttala@gmail.com>
2024-04-24 05:17:47 +0200

    save

--- gnngls/datasets.py
+++ gnngls/datasets.py
@@ -57,6 +57,99 @@ def set_labels2(G):
 #             else:
 #                 pass
 
+def directed_string_graph(G1):
+    n = G1.number_of_nodes()
+    m = n*(n-1)
+
+    i, j = 0, 1
+    ss = []
+    st = []
+    ts = []
+    tt = []
+    pp = []
+    edge_id = dict()
+    # features = []
+    # regret = []
+    # in_solution = []
+    for idx, edge in enumerate(G1.edges()):
+        edge_id[edge] = idx
+    #     features.append(G1.edges[edge]['weight'])
+    #     regret.append(G1.edges[edge]['regret'])
+    #     in_solution.append(G1.edges[edge]['in_solution'])
+
+    # features = np.vstack(features)
+    # regret = np.vstack(regret)
+    # in_solution = np.vstack(in_solution)
+    print(edge_id)
+    set_list = set()
+    for idx in range(m):
+        # parallel
+        if (i, j, j, i) not in set_list:
+            set_list.add((i, j, j, i))
+            set_list.add((j, i, i, j))
+            pp.append((edge_id[(i, j)], edge_id[(j, i)]))
+        # src to src
+        for v in range(n):
+            if v != i and v != j and (i, j, i, v) not in set_list:
+                set_list.add((i, j, i, v))
+                set_list.add((i, v, i, j))
+                ss.append((edge_id[(i, j)], edge_id[(i, v)]))
+        # src to target
+        for v in range(n):
+            if v != i and v != j and (i, j, v, i) not in set_list:
+                set_list.add((i, j, v, i))
+                set_list.add((v, i, i, j))
+                st.append((edge_id[(i, j)], edge_id[(v, i)]))
+        # target to src
+        for v in range(n):
+            if v != i and v != j and (i, j, j, v) not in set_list:
+                set_list.add((i, j, j, v))
+                set_list.add((j, v, i, j))
+                ts.append((edge_id[(i, j)], edge_id[(j, v)]))
+        # target to target
+        for v in range(n):
+            if v != i and v != j and (i, j, v, j) not in set_list:
+                set_list.add((i, j, v, j))
+                set_list.add((v, j, i, j))
+                tt.append((edge_id[(i, j)], edge_id[(v, j)]))
+        
+
+        j += 1
+        if i == j:
+            j += 1
+        if j == n:
+            j = 0
+            i += 1
+    edge_types = {('node', 'ss', 'node'): ss,
+              ('node', 'st', 'node'): st,
+              ('node', 'ts', 'node'): ts,
+              ('node', 'tt', 'node'): tt,
+              ('node', 'pp', 'node'): pp}
+    
+    G2 = dgl.heterograph(edge_types)
+    G2 = G2.add_reverse_edges(G2)
+
+    # G2.ndata['weight'] = torch.tensor(features, dtype=torch.float32)
+    # G2.ndata['regret'] = torch.tensor(regret, dtype=torch.float32)
+    # G2.ndata['in_solution'] = torch.tensor(in_solution, dtype=torch.float32)
+
+    # Print the number of nodes and edges
+    print("Number of nodes:", G2.number_of_nodes())
+    print("Number of edges:", G2.number_of_edges())
+
+    # Print node and edge features
+    print("Node features:")
+    print(G2.ndata)
+    print("Edge features:")
+    print(G2.edata)
+
+    # Print graph structure
+    print("Graph structure:")
+    print(G2)
+    for etype in G2.canonical_etypes:
+        print(etype)
+        print(G2.adj(etype=etype))
+
 class TSPDataset(torch.utils.data.Dataset):
     def __init__(self, instances_file, scalers_file=None, feat_drop_idx=[]):
         if not isinstance(instances_file, pathlib.Path):
@@ -77,13 +170,7 @@ class TSPDataset(torch.utils.data.Dataset):
 
         # only works for homogenous datasets
         G = nx.read_gpickle(self.root_dir / self.instances[0])
-        lG = nx.line_graph(G)
-        #lG = lG.to_undirected()
-        for n in lG.nodes:
-            lG.nodes[n]['e'] = n
-        
-        # why he add the id number of the edegs in the G graph
-        self.G = dgl.from_networkx(lG, node_attrs=['e'])
+        directed_string_graph(G)
         
 
     
@@ -109,6 +196,7 @@ class TSPDataset(torch.utils.data.Dataset):
             features.append(G.edges[e]['weight'])
             regret.append(G.edges[e]['regret'])
             in_solution.append(G.edges[e]['in_solution'])
+
         features = np.vstack(features)
         features_transformed = self.scalers['weight'].transform(features)
         features_transformed = np.delete(features_transformed, self.feat_drop_idx, axis=1)
@@ -119,7 +207,8 @@ class TSPDataset(torch.utils.data.Dataset):
         H = copy.deepcopy(self.G)
         H.ndata['weight'] = torch.tensor(features_transformed, dtype=torch.float32)
         H.ndata['regret'] = torch.tensor(regret_transformed, dtype=torch.float32)
-        H.ndata['regret'] = torch.nn.functional.softmax(H.ndata['regret'], dim=1)
 
-        H.ndata['in_solution'] = torch.tensor(regret, dtype=torch.float32)
+        H.ndata['in_solution'] = torch.tensor(in_solution, dtype=torch.float32)
         return H
+
+
--- test_dgl.py
+++ test_dgl.py
@@ -1,37 +1,108 @@
-import networkx as nx
+import copy
+import pathlib
+import pickle
+
 import dgl
+import networkx as nx
+import numpy as np
+import torch
+import torch.utils.data
 
-# Step 1: Convert NetworkX graph to DGL graph
-def nx_to_dgl(nx_graph):
-    dgl_graph = dgl.from_networkx(nx_graph, edge_attrs=None)
-    return dgl_graph
 
-# Step 2: Create line graph from DGL graph
-def create_line_graph(dgl_graph):
-    line_graph = dgl.line_graph(dgl_graph, backtracking=False)
-    return line_graph
+def directed_string_graph(G1):
+    n = G1.number_of_nodes()
+    m = n*(n-1)
 
-# Step 3: Add different types of edges
-def add_edge_types(line_graph, original_graph):
-    num_nodes = original_graph.number_of_nodes()
-    num_edges = original_graph.number_of_edges()
+    i, j = 0, 1
+    ss = []
+    st = []
+    ts = []
+    tt = []
+    pp = []
+    edge_id = dict()
+    # features = []
+    # regret = []
+    # in_solution = []
+    for idx, edge in enumerate(G1.edges()):
+        edge_id[edge] = idx
+    #     features.append(G1.edges[edge]['weight'])
+    #     regret.append(G1.edges[edge]['regret'])
+    #     in_solution.append(G1.edges[edge]['in_solution'])
+
+    # features = np.vstack(features)
+    # regret = np.vstack(regret)
+    # in_solution = np.vstack(in_solution)
+    print(edge_id)
+    set_list = set()
+    for idx in range(m):
+        # parallel
+        if (i, j, j, i) not in set_list:
+            set_list.add((i, j, j, i))
+            set_list.add((j, i, i, j))
+            pp.append((edge_id[(i, j)], edge_id[(j, i)]))
+        # src to src
+        for v in range(n):
+            if v != i and v != j and (i, j, i, v) not in set_list:
+                set_list.add((i, j, i, v))
+                set_list.add((i, v, i, j))
+                ss.append((edge_id[(i, j)], edge_id[(i, v)]))
+        # src to target
+        for v in range(n):
+            if v != i and v != j and (i, j, v, i) not in set_list:
+                set_list.add((i, j, v, i))
+                set_list.add((v, i, i, j))
+                st.append((edge_id[(i, j)], edge_id[(v, i)]))
+        # target to src
+        for v in range(n):
+            if v != i and v != j and (i, j, j, v) not in set_list:
+                set_list.add((i, j, j, v))
+                set_list.add((j, v, i, j))
+                ts.append((edge_id[(i, j)], edge_id[(j, v)]))
+        # target to target
+        for v in range(n):
+            if v != i and v != j and (i, j, v, j) not in set_list:
+                set_list.add((i, j, v, j))
+                set_list.add((v, j, i, j))
+                tt.append((edge_id[(i, j)], edge_id[(v, j)]))
+        
+
+        j += 1
+        if i == j:
+            j += 1
+        if j == n:
+            j = 0
+            i += 1
+    edge_types = {('node', 'ss', 'node'): ss,
+              ('node', 'st', 'node'): st,
+              ('node', 'ts', 'node'): ts,
+              ('node', 'tt', 'node'): tt,
+              ('node', 'pp', 'node'): pp}
     
-    for i in range(num_edges):
-        # source-target edge
-        line_graph.add_edges(i, num_edges + i)
-        # target-source edge
-        line_graph.add_edges(num_edges + i, i)
-        # source-source edge
-        line_graph.add_edges(i, i)
-        # target-target edge
-        line_graph.add_edges(num_edges + i, num_edges + i)
-    return line_graph
+    G2 = dgl.heterograph(edge_types)
+    G2 = G2.add_reverse_edges(G2)
 
-# Example usage:
-# Assuming you have a directed NetworkX graph named nx_graph
-nx_graph = nx.complete_graph(3).to_directed()  # Example directed complete graph
-dgl_graph = nx_to_dgl(nx_graph)
-print(dgl_graph.edges())
-for edge in zip(*dgl_graph.edges()):
-    print(edge)
-    
+    # G2.ndata['weight'] = torch.tensor(features, dtype=torch.float32)
+    # G2.ndata['regret'] = torch.tensor(regret, dtype=torch.float32)
+    # G2.ndata['in_solution'] = torch.tensor(in_solution, dtype=torch.float32)
+
+    # Print the number of nodes and edges
+    print("Number of nodes:", G2.number_of_nodes())
+    print("Number of edges:", G2.number_of_edges())
+
+    # Print node and edge features
+    print("Node features:")
+    print(G2.ndata)
+    print("Edge features:")
+    print(G2.edata)
+
+    # Print graph structure
+    print("Graph structure:")
+    print(G2)
+    for etype in G2.canonical_etypes:
+        print(etype)
+        print(G2.adj(etype=etype))
+
+
+complete_graph = nx.complete_graph(4).to_directed()
+print(complete_graph.edges())
+directed_string_graph(complete_graph)
