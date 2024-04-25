ce4610c832c21e455783bd42c339359458af5858
walidgeuttala <walidgeuttala@gmail.com>
2024-04-25 15:13:06 +0200

    save

--- job.sh
+++ job.sh
@@ -18,8 +18,12 @@
 #python train.py ../generated ../model_result --use_gpu
 #python test_me.py
 #python preprocess_dataset2.py ../generatedn2000
-python train2.py ../tsp_n5900 ../model_result_n5900 --use_gpu
-#python test.py ../tsp_n5900/test2.txt ../model_result_n5900/Apr19_19-12-12_0b1f56032356479d99089a34b7ec6922/checkpoint_best_val.pt ../run_result_n5900 regret_pred ../test_do --use_gpu
+#python train2.py ../tsp_n5900 ../model_result_n5900 --use_gpu
+python test.py ../tsp_n5900/test2.txt ../model_result_n5900/Apr25_14-21-43_167db68588d5493fbd47a7a145fa3e02/checkpoint_best_val.pt ../run_result_n5900 regret_pred ../test_model4 --use_gpu
 #python generate_instances.py 8000 128 ../tsplib95_10000_instances_64_node/tsp_all_instances_adj_tour_cost.txt ../generatedn8000
 #python test_regret.py
 #python remove_extra_edges.py
+
+
+
+#Apr25_14-21-43_167db68588d5493fbd47a7a145fa3e02
