4ac8586d3a43840ac55e89fdc91671a2af4249d0
walidgeuttala <walidgeuttala@gmail.com>
2024-05-13 17:04:15 +0200

    save

--- testing3.py
+++
@@ -1,9 +0,0 @@
-from gnngls import datasets
-import torch
-val_data = datasets.TSPDataset(f"../tsp_n5900/val.txt")
-
-ans = 0
-for idx in range(len(val_data)):
-    ans += val_data[idx].ndata['regret'].mean()
-
-print(ans/len(val_data))
